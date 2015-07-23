b5bf6a94fc17c286fafd628644bd95c4b5c9ece2
legit133 <jellopy620@gmail.com>
2015-07-24 00:21:34 +0800

    Encrpytino done

--- JM_P1459816.pl
+++ JM_P1459816.pl
@@ -27,12 +27,7 @@ sub Cipher{
 
 	if($userInput eq 'e'){
 		encrypt();
-		#print "Type output file:";
-		#chomp(my $file =<STDIN>);
-		#print $file;	
-		#open my $file, '>', $file ;
-		#print {$file} $msg . "\n";
-		#close $file;
+
 	} elsif ($userInput eq 'd') {
 		print "Type the file you want to decrypt: ";
 	
@@ -48,18 +43,51 @@ sub encrypt{
 		chomp (my $msg = <STDIN>);
 		my @emsg_array = split / /, $msg; #split the message by spaces
 		my $emsg =''; #Encrypted message will store in here
+		
 
-		foreach $emsg_array(@emsg_array){
+		for $emsg_array(@emsg_array){ #start of loop
 			#print "$emsg_array";
-			my $chck_word_length = length($emsg_array); #This is to check the length of the message
-			#print "$chck_word_length";
-			my $letters_word_length =($emsg_array =~ tr/[A-Za-z]//); #Check for alphabet character length for shift
-			print "$letters_word_length";
+			my $count_msg = length($emsg_array); #This is to count the length of the $emsg_array
+			#print "$count_msg";
+			my $countAlphaMsg =($emsg_array =~ tr/[A-Za-z]//); #Count only alphabets in $emsg_array
+			print "$countAlphaMsg";
 
-			for($i=0 $i<chck_word_length;$i++){
-				my $encrypt_letter substr($emsg_array, $i, 1);
+			
+			my $encrypt_word ='';
+			for($i=0; $i<$count_msg; $i++){
+				my $convert_Msg = substr($emsg_array, $i, 1);
+
+				if($convert_Msg =~ m/[A-Za-z]/){ #check if letter is an aplhabet char or not
+					$convert_Msg = ord($convert_Msg); #Convert all letters into decimal
+					$encrypt_Msg = $convert_Msg+$countAlphaMsg;
+
+					
+				if ((($convert_Msg>=64 and $convert_Msg<=90) and $encrypt_Msg>90) or (($convert_Msg>=97 and $convert_Msg<= 122) and $encrypt_Msg>122)){ #To make an roundabout 
+					$encrypt_Msg = $encrypt_Msg - 26;
+				}
+
+				$encrypt_Msg =chr($encrypt_Msg);
+				$encrypt_word = $encrypt_word.$encrypt_Msg;
+				}else{
+					$encrypt_word = $encrypt_word.$convert_Msg;
+				}
 
 			}
-		}
+			if ($emsg eq ''){
+				$emsg = $emsg.$encrypt_word;
 
-}
+			}else {
+				$emsg = $emsg. ' '.$encrypt_word;
+			}
+		}
+			
+				print "\nType output file: ";
+	$output = <STDIN>;
+
+	open(FILEHANDLE, ">$output");
+	print FILEHANDLE "$emsg\n";
+	close FILEHANDLE;
+	exit 0;
+		
+	}
+
